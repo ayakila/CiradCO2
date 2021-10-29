16b5825a844a97f7054aeef91682cc20d1fa4dc9
aya_kilani <aya.kilani@redlean.io>
2021-10-29 17:33:26 +0200

    fix bugs

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -2,21 +2,15 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 declare interface RouteInfo {
-    path: string;
-    title: string;
-    icon: string;
-    class: string;
+  path: string;
+  title: string;
+  icon: string;
+  class: string;
 }
 export const ROUTES: RouteInfo[] = [
-    { path: '/dashboard', title: 'Acceuil',  icon: 'ni-tv-2 text-primary', class: '' },
-   // { path: '/icons', title: 'Icons',  icon:'ni-planet text-blue', class: '' },
-    //{ path: '/maps', title: 'Maps',  icon:'ni-pin-3 text-orange', class: '' },
-   // { path: '/user-profile', title: 'User profile',  icon:'ni-single-02 text-yellow', class: '' },
-   { path: '/calcul-ges', title: 'Calcul Ges',  icon:'fas fa-calculator text-pink', class: '' }
-   { path: '/mes-resultats', title: 'Mes résultats',  icon:'ni-bullet-list-67 text-red', class: '' },
-    //{ path: '/login', title: 'Login',  icon:'ni-key-25 text-info', class: '' },
-    //{ path: '/register', title: 'Register',  icon:'ni-circle-08 text-pink', class: '' },
-
+  { path: '/dashboard', title: 'Acceuil', icon: 'ni-tv-2 text-primary', class: '' },
+  { path: '/calcul-ges', title: 'Calcul Ges', icon: 'fas fa-calculator text-pink', class: '' },
+  { path: '/mes-resultats', title: 'Mes résultats', icon: 'ni-bullet-list-67 text-red', class: '' }
 ];
 
 @Component({
@@ -35,6 +29,6 @@ export class SidebarComponent implements OnInit {
     this.menuItems = ROUTES.filter(menuItem => menuItem);
     this.router.events.subscribe((event) => {
       this.isCollapsed = true;
-   });
+    });
   }
 }
